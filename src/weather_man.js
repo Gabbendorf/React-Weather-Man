9e03de59fa1ce8d350845f98eb3735fa7b645421
Gabba <gmedas@8thlight.com>
2018-03-28 14:58:16 +0100

    Extracts method that gets data from API and tests mocked data fetch.

--- src/weather_man.js
+++ src/weather_man.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'whatwg-fetch'
+import 'whatwg-fetch';
 const rootUrl = 'http://api.openweathermap.org/data/2.5/weather?q=';
 const APIKey = '&APPID=81ebd306a97bf6e5342257562b958514';
 
@@ -8,19 +8,28 @@ class WeatherMan extends React.Component {
 
   constructor(props) {
     super(props)
-    this.state = {city: "",
-                  weather: ""
-                 }
+    this.state = {
+      city: "",
+      weather: "",
+    }
+  }
+
+  getDataFor(city) {
+    const url = rootUrl + city + APIKey;
+    return fetch(url)
+      .then(response => {
+	return response.json();
+      });
   }
 
   componentWillMount() {
-    const url = rootUrl + this.props.city + APIKey;
-    fetch(url)
-      .then(response => response.json())
-      .then(cityData => this.setState({ city: cityData.name,
-	                                weather: cityData.weather[0].description
-      })
-    );
+    this.getDataFor(this.props.city)
+      .then(data => {
+	this.setState({
+	  city: data.name,
+	  weather: data.weather[0].description,
+	});
+      });
   }
 
   render() {
--- test/weather_man.spec.js
+++ test/weather_man.spec.js
@@ -1,6 +1,24 @@
-import React from 'react'
-import { WeatherMan } from '../src/weather_man'
+import React from 'react';
+import fetchMock from 'fetch-mock';
+import { WeatherMan } from '../src/weather_man';
+
+export const dataForPadovaPromised = {
+  city: "Padua",
+  weather: "sunny"
+}
 
 test('renders without crashing', () => {
   mount(<WeatherMan city="London"/>);
+})
+
+test('checks API is called', () => {
+  const weatherMan = new WeatherMan();
+  fetchMock.get("*", dataForPadovaPromised);
+
+  weatherMan.getDataFor("padova")
+    .then(dataForPadovaReceived => {
+      expect(dataForPadovaReceived).toEqual(dataForPadovaPromised);
+    });
+
+  fetchMock.restore();
 })
