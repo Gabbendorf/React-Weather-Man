212e500ced2499c240ff44eba6968269a4408a42
Gabba <gmedas@8thlight.com>
2018-03-29 16:02:50 +0100

    Deletes line.

--- src/app.js
+++ src/app.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { WeatherMan } from './weather_man';
 
-
 ReactDOM.render(
   <WeatherMan city="padova"/>,
   document.getElementById('app')
