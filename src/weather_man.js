b54bc6783b2e6cb8c2dc14b450eb995bc6c88be4
Gabba <gmedas@8thlight.com>
2018-04-04 11:54:20 +0100

    Message is set in same setState where weather is retrieved to guarantee it updates when button is clicked.

--- src/weather_man.js
+++ src/weather_man.js
@@ -8,11 +8,10 @@ class WeatherMan extends React.Component {
     super(props)
     this.state = {
       city: "",
-      weather: "",
-      message: "",
+      weatherForecast: "",
     }
     this.registerCity = this.registerCity.bind(this);
-    this.updateMessage = this.updateMessage.bind(this);
+    this.updateWeatherForecast = this.updateWeatherForecast.bind(this);
   }
 
   registerCity(event) {
@@ -21,32 +20,22 @@ class WeatherMan extends React.Component {
     })
   }
 
-  registerData() {
+  updateWeatherForecast() {
     getDataFor(this.state.city)
       .then(data => {
 	this.setState({
-	  city: data.name,
-	  weather: data.weather[0].description,
+	  weatherForecast: "The weather in " + this.state.city + " now is: " + data.weather[0].description.toUpperCase(),
 	})
-      });
-  }
-
-  updateMessage() {
-    this.registerData();
-    this.setState({
-      message: "The weather in " + this.state.city + " now is: " + this.state.weather
-    })
+      })
   }
 
   render() {
     return (
       <div>
-        <label>
-          <h1>How is the weather now in:</h1>
-          <input type="text" value={this.state.city} onChange={this.registerCity} autoFocus="autofocus" />
-        </label>
-        <button onClick={this.updateMessage}>Submit</button>
-        <p>{this.state.message}</p>
+        <h1>How is the weather now in:</h1>
+        <input type="text" value={this.state.city} onChange={this.registerCity} autoFocus="autofocus" />
+        <button onClick={this.updateWeatherForecast}>Submit</button>
+        <p>{this.state.weatherForecast}</p>
       </div>
     );
   }
